a0e5ab5562742f92155961c823d55518608aca92
Emil Johansson <emil.sweden@gmail.com>
2018-07-09 10:15:02 +0200

    Add proper docstrings to PyTandem functions.
    Should not be necessary to dig in the c++ code to understand the
    functions exposed to python.

--- PyTandem.cpp
+++ PyTandem.cpp
@@ -1,13 +1,13 @@
 #include "tandem_function_interface.h"
 #include "conversions.h"
 #include <vector>
+#include <string>
 
 using namespace std;
-namespace py = boost::python;
-
+using namespace boost::python;
 BOOST_PYTHON_MODULE(PyTandem) // 
 {
-  using namespace boost::python;
+  docstring_options local_docstring_options(true, true, false);
   // register the vec-to-python converter
   to_python_converter<
     vector<double>,
@@ -21,7 +21,36 @@ BOOST_PYTHON_MODULE(PyTandem) //
   // Register python to mat
   std_mat_from_python_list();
   def("tandem", tandem,
-      return_value_policy<return_by_value>());
+      return_value_policy<return_by_value>(),
+      "Generate N-Representable density matrices\n\n"
+      "Generates N-Representable density matrices by sampling the N-particle wave function\n"
+      "space and reducing the wave functions to 2RDMs\n\n"
+      "# Arguments:\n"
+      "    num_particles (int)\n"
+      "    num_examples (int)\n"
+      "    num_orbitals (int)\n"
+      "    distribution (string) --- Specify from what distribution as a function of N-particle\n"
+      "        basis index the N-particle wave functions are to be drawn from.\n"
+      "        On of 'uniform', 'one_over_x-P', 'exponential-T'. P and T are float parameters.\n"
+      "        'one_over_x-P' samples wave functions coefficient according to 1/x^P. 'exponential-T'\n"
+      "        according to e^(-x/T)\n\n"
+      "# Returns:\n"
+      "    2RDMs (2D list) --- The log-Cholesky decomposition of the 2D matrix representation\n"
+      "        of the full two particle reduced density matrix. See Nakata(2017) page 16\n"
+      "        for specification of how the rank 4 tensor is transformed to a rank 2 matrix.\n"
+      "        See Pinheiro(1996) for details on the log-Cholesky decomposition.\n"
+      );
   def("tandem_on_wf", tandem_on_wf,
-      return_value_policy<return_by_value>());
+      return_value_policy<return_by_value>(),
+      "Get 2RDM of a wave function\n\n"
+      "# Arguments:\n"
+      "    wave_function (list) --- The wave function to be reduced\n"
+      "    num_particles (int)\n"
+      "    num_orbitals (int)\n\n"
+      "# Return:\n"
+      "    2RDM (list) --- The log-Cholesky decomposition of the 2D matrix representation\n"
+      "        of the full two particle reduced density matrix. See Nakata(2017) page 16\n"
+      "        for specification of how the rank 4 tensor is transformed to a rank 2 matrix.\n"
+      "        See Pinheiro(1996) for details on the log-Cholesky decomposition.\n"
+      );
 }
