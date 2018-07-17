c8a889c62f7229ba12a88e308389aded0d745b84
Emil Johansson <emil.sweden@gmail.com>
2018-07-17 16:29:20 +0200

    Add HartreeFock wf generation and generalize HF testing
    It is now the tests/Makefiles responsibility to keep track of what
    files to compile and what tests to run.

---
+++ tests/hf_test.cpp
@@ -0,0 +1,26 @@
+// hartree fock state construction test
+#include "../hartree_fock.h"
+#include <vector>
+#include <iostream>
+#include <math.h>
+#define BOOST_TEST_DYN_LINK        // this is optional
+#define BOOST_TEST_MODULE HartreeFockTest
+#include <boost/test/included/unit_test.hpp>  // include this to get main()
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE( hartree_fock_test )
+{
+  size_t num_particles = 2;
+  size_t num_orbitals = 3;
+  vector<vector<double>> D(num_particles, vector<double>(num_orbitals, 0));
+  D.at(0).at(0) = -1 / sqrt(2); D.at(1).at(0) = 1 / sqrt(2);
+  D.at(0).at(1) = 1 / sqrt(4); D.at(1).at(1) = 1 / sqrt(4);
+  D.at(0).at(2) = 1 / sqrt(4); D.at(1).at(2) = 1 / sqrt(4);
+  vector<double> result = hf_wf_from_D(D, num_orbitals, num_particles);
+  vector<double> expected_result = {1 / sqrt(2), 1 / sqrt(2), 0};
+  BOOST_CHECK(result.size() == expected_result.size());
+  for (size_t i = 0; i < result.size(); i++){
+    BOOST_CHECK(abs(result.at(i) - expected_result.at(i)) < 1e-2);
+  }
+}
