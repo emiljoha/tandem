01526c4c4c32d7910eedfc60b866e556cc87d15a
Emil Johansson <emil.sweden@gmail.com>
2018-07-17 15:45:49 +0200

    Add use travic-ci and boost.test to test zero_spin configuration
    Initial step before converting all tandem c++ tests into boost.test +
    travis-ci automated tests.

--- tests/zero_spin.cpp
+++ tests/zero_spin.cpp
@@ -2,11 +2,13 @@
 #include <boost/math/special_functions/binomial.hpp>
 #include <vector>
 #include "../tandem_function_interface.h"
+#define BOOST_TEST_DYN_LINK        // this is optional
+#define BOOST_TEST_MODULE ZeroSpinTest
+#include <boost/test/included/unit_test.hpp>  // include this to get main()
 
 using namespace std;
 
-int main() {
-  cout << "zero_spin distribution test... ";
+bool test_zero_spin_tandem() {
   // Basic info
   const int num_orbitals = 20;
   const int num_particles = 4;
@@ -16,6 +18,10 @@ int main() {
       boost::math::binomial_coefficient<double>(num_orbitals, num_particles);
   vector<vector<double>> res = tandem(num_particles, num_examples,
 				      num_orbitals, distribution);
-  cout << " PASSED!" << endl;
-  return 0;
+  return true;
+}
+
+BOOST_AUTO_TEST_CASE( zero_spin_test )
+{
+  BOOST_CHECK(test_zero_spin_tandem());
 }
