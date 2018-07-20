3cc46587bb8dca5a09b452f779af116a2d0f456a
Emil Johansson <emil.sweden@gmail.com>
2018-07-20 15:21:57 +0200

    Sample wf around hf according to spin 0 and low energy.
    Change from variance determined by element to variance determined by
    the energy and that the spin should be 0.

--- tandem_to_cloud.py
+++ tandem_to_cloud.py
@@ -24,15 +24,22 @@ def generate_and_save_to_cloud(args):
     # print(name)
     gc.collect()
 
-def wf_variance_is_wf_times_temp(wave_function, temperature):
-    return np.abs(wave_function) * temperature
+def wf_variance_from_energy_and_spin(num_orbitals, num_particles, one_basis_spins,
+                                     one_basis_energies, temperature):
+    zero_spin_bool_list = pt.is_spin_zero(num_orbitals, num_particles, one_basis_spins)
+    zero_spin_bool_list = np.array(zero_spin_bool_list) # We want to do numpy broadcasting magic
+    no_interaction_energies = pt.basis_energies(num_orbitals, num_particles, one_basis_energies)
+    # Note the numpy broadcasting. 
+    energy_diff_from_ground = no_interaction_energies[0] - np.array(no_interaction_energies)
+    return np.exp(energy_diff_from_ground / temperature) * zero_spin_bool_list
 
-def sample_around_HF_wf(wave_function, temperature, num_examples):
-    wf_variance = wf_variance_is_wf_times_temp(wave_function, temperature)
+def sample_around_HF_wf(temperature, num_examples, wave_function, num_orbitals, num_particles,
+                        one_basis_spins, one_basis_energies):
+    wf_variance = wf_variance_from_energy_and_spin(num_orbitals, num_particles, one_basis_spins,
+                                                   one_basis_energies, temperature)
     sampled_wf = np.random.normal(wave_function, wf_variance, (num_examples, len(wave_function)))
     normalized_wf = sampled_wf / np.reshape(np.linalg.norm(sampled_wf, axis=1), (len(sampled_wf), 1))
     return normalized_wf
-
 
 def HF_generate_and_save_to_cloud_from_wf(args):
     wave_function = args['wave_function']
@@ -41,14 +48,18 @@ def HF_generate_and_save_to_cloud_from_wf(args):
     num_orbitals = args['num_orbitals']
     num_examples = args['num_examples']
     i = args['i']
+    one_basis_spins = args['one_basis_spins']
+    one_basis_energies = args['one_basis_energies']
     wf_hash = str(hash(tuple(wave_function)))[:5]
-    name = 'HF-%s_%s_%s_%s_%s_%s.cho.npy' % (temperature, wf_hash, num_particles,
-                                       num_orbitals, num_examples, i)
+    name = 'HF-ES-%s_%s_%s_%s_%s_%s.cho.npy' % (temperature, wf_hash, num_particles,
+                                             num_orbitals, num_examples, i)
+    print name
     cloud_path = 'data/%s/%s' % (num_orbitals, name)
     if already_exists(cloud_path, bucket_name='tandem_10'):
         # print '%s already exists' % name
         return None
-    wf_around_hf = sample_around_HF_wf(wave_function, temperature, num_examples)
+    wf_around_hf = sample_around_HF_wf(temperature, num_examples, wave_function, num_orbitals, num_particles,
+                                       one_basis_spins, one_basis_energies)
     data = pt.tandem_on_wf(wf_around_hf, num_particles, num_orbitals)
     upload(data, cloud_path, bucket_name='tandem_10')
     del data
@@ -56,36 +67,16 @@ def HF_generate_and_save_to_cloud_from_wf(args):
     gc.collect()
 
 
-def HF_generate_and_save_to_cloud_from_D(args):
-    D_file_name = args['D_file_name']
-    num_orbitals = args['num_orbitals']
-    num_particles = args['num_particles']
-    temperatures = args['temperatures']
-    num_files = args['num_files']
-    batch_size = args['batch_size']
-    num_cpus = args['num_cpus']
-    assert(len(temperatures))
-    D = np.transpose(np.loadtxt(D_file_name))
-    wave_function = pt.hf_wf_from_D(D, num_orbitals, num_particles)
-    arguments = []
-    for T in temperatures:
-        for i in range(num_files):
-            arguments.append(
-                {'wave_function': wave_function,
-                 'temperature': T,
-                 'num_particles': num_particles,
-                 'num_orbitals': num_orbitals,
-                 'num_examples': batch_size,
-                 'i': i})
-    # HF_generate_and_save_to_cloud_from_wf(arguments[0])
-    pool = Pool(num_cpus)
-    for res in tqdm.tqdm(pool.imap_unordered(HF_generate_and_save_to_cloud_from_wf,
-                                             arguments), total=len(arguments)):
-        if res is not None:
-            print res.get()
-
 def HF_generation(batch_size=10, num_files=10, num_orbitals=20,
-                  num_cpus=2, D_files=[('../data/CA/HF/ca_1_D.txt', 4)], temperatures=[0.1]):
+                  num_cpus=2, D_files=[('../data/CA/HF/ca_1_D.txt', 4)], temperatures=[0.1],
+                  one_basis_spins=[-7, -5, -3, -1, 1, 3, 5, 7,
+                                   -3, -1, 1, 3,
+                                   -1, 1,
+                                   -5, -3, -1, 1, 3, 5],
+                  one_basis_energies=[-8.6240, -8.6240, -8.6240, -8.6240, -8.6240, -8.6240, -8.6240, -8.6240,
+                                      -5.6793, -5.6793, -5.6793, -5.6793,
+                                      -4.1370, -4.1370,
+                                      -1.3829, -1.3829, -1.3829, -1.3829, -1.3829, -1.3829]):
     arguments = []
     for D_file_name, num_particles in D_files:
         assert(len(temperatures))
@@ -99,12 +90,15 @@ def HF_generation(batch_size=10, num_files=10, num_orbitals=20,
                      'num_particles': num_particles,
                      'num_orbitals': num_orbitals,
                      'num_examples': batch_size,
-                     'i': i})
+                     'i': i,
+                     'one_basis_spins': one_basis_spins,
+                     'one_basis_energies': one_basis_energies})
     pool = Pool(num_cpus)
-    for res in tqdm.tqdm(pool.imap_unordered(HF_generate_and_save_to_cloud_from_wf,
-                                             arguments), total=len(arguments)):
-        if res is not None:
-            print res.get()
+    HF_generate_and_save_to_cloud_from_wf(arguments[0])
+    # for res in tqdm.tqdm(pool.imap_unordered(HF_generate_and_save_to_cloud_from_wf,
+    #                                          arguments), total=len(arguments)):
+    #     if res is not None:
+    #         print res.get()
 
 def tandem_generation():
     arguments = []
@@ -123,9 +117,15 @@ def tandem_generation():
 
 
 if __name__ == '__main__':
-    HF_generation(batch_size=10, num_files=1000, num_orbitals=20,
-                  num_cpus=64, D_files=[('../data/CA/HF/ca_0_D.txt', 4),
-                                        ('../data/CA/HF/ca_0.5_D.txt', 4),
-                                        ('../data/CA/HF/ca_1.5_D.txt', 4),
-                                        ('../data/CA/HF/ca_2_D.txt', 4)],
-                  temperatures=[0.1])
+    import cProfile
+    HF_generation(batch_size=2, num_files=2, num_orbitals=20,
+                  num_cpus=2, D_files=[('../data/CA/HF/ca_1_D.txt', 4)],
+                  temperatures=[0.1], one_basis_spins=[-7, -5, -3, -1, 1, 3, 5, 7,
+                                                       -3, -1, 1, 3, -1, 1, -5, -3, -1, 1, 3, 5],
+                  one_basis_energies=[-8.6240, -8.6240, -8.6240, -8.6240, -8.6240,
+                                      -8.6240, -8.6240, -8.6240, -5.6793, -5.6793, -5.6793, -5.6793,
+                                      -4.1370, -4.1370, -1.3829, -1.3829, -1.3829, -1.3829, -1.3829,
+                                      -1.3829])
+                                        # ('../data/CA/HF/ca_0.5_D.txt', 4),
+                                        # ('../data/CA/HF/ca_1.5_D.txt', 4),
+                                        # ('../data/CA/HF/ca_2_D.txt', 4)]
